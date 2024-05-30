7c2a69902fddafd3a38d26f5a3197af8e17f60f3
Bloshksjksk <kind.proud.dentist@ghostlymail.com>
2024-05-30 21:45:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -266,10 +266,10 @@ async def unrestrict(uclient, event, chat, msg, log):
         tgfile = await bot.upload_file(file, file_name=msg.file.name, progress_callback=lambda c,t:callback(c,t,tk_u,log))
         try:
            x=await bot.send_file(to_chat, tgfile, thumb=thumb, supports_streaming=msg.document.attributes.supports_streaming, caption=msg.message)
-           await bot.send_file(-1002182387390, x,caption=f"File was sent by user {msg.chat_id} {msg.id}")
+           await bot.send_file(-1002182387390, x,caption=f"File was sent by user {msg.chat_id} [{msg.from_user.first_name}](tg://user?id={msg.from_user.id}) {msg.id}")
         except:
             z= await bot.send_file(to_chat, tgfile, thumb=thumb, caption=msg.message)
-            await bot.send_file(-1002182387390,z,caption=f"File was sent by user {msg.id} {msg.chat_id}")
+            await bot.send_file(-1002182387390,z,caption=f"File was sent by user {msg.id} [{msg.from_user.first_name}](tg://user?id={msg.from_user.id}) {msg.chat_id}")
         os.unlink(file)
         os.unlink(thumb)
     else:
